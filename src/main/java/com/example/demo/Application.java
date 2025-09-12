fd13b4bc3bdd2052c49eaebcc91d46053886bf16
Arwynhwyl <arwynhwyl@Arwynhwyls-MacBook-Air-2.local>
2025-09-12 10:17:32 +0700

    4.6

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -18,9 +18,8 @@ public class Application {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/events/**")
-						.allowedOrigins("http://localhost:5173")
-						.exposedHeaders("x-total-count");
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:5173");
 			}
 		};
 	}
