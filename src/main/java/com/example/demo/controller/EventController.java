352727a9f80359e1798e18afa13544127cc1bbdc
Arwynhwyl <arwynhwyl@Arwynhwyls-MacBook-Air-2.local>
2025-09-12 09:51:52 +0700

    fix

--- src/main/java/com/example/demo/controller/EventController.java
+++ src/main/java/com/example/demo/controller/EventController.java
@@ -4,16 +4,18 @@ import com.example.demo.entity.Event;
 import jakarta.annotation.PostConstruct;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/events")
 public class EventController {
     List<Event> eventList;
 
@@ -88,7 +90,7 @@ public class EventController {
                 .build());
     }
 
-    @GetMapping("/events")
+    @GetMapping("")
     public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage,
                                          @RequestParam(value = "_page", required = false) Integer page) {
         perPage = perPage == null ? eventList.size() : perPage;
@@ -105,7 +107,7 @@ public class EventController {
         return ResponseEntity.ok(output);
     }
 
-    @GetMapping("events/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<?> getEvent(@PathVariable("id") Long id) {
         Event output = null;
         for (Event event : eventList) {
